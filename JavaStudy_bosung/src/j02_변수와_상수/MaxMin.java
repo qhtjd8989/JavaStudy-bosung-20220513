b04568250024a94a161b1e23267d42b7086f0477
qhtjd8989 <qhtjd8899@naver.com>
2022-05-13 21:09:49 +0900

    프로젝트 첫 커밋 변수와 상수 수업 코드

---
+++ JavaStudy_bosung/src/j02_변수와_상수/MaxMin.java
@@ -0,0 +1,25 @@
+package j02_변수와_상수;
+
+public class MaxMin {
+
+	public static void main(String[] args) {
+		
+		final int MAX_NUMBER; // 상수가 둘 이상의 단어가 들어간 변수명이면  _로 구분한다 (상수는 대문자로 표기)
+		MAX_NUMBER = 100;
+		final int MIN_NUMBER;
+		MIN_NUMBER = 0;
+		
+		final double PI = 3.14;
+		
+//		MAX = 1000;
+//		MIN = 100;
+//		
+//		System.out.println("최대값: " + MAX);
+//		System.out.println("최소값: " + MIN);
+		
+		System.out.println(MAX_NUMBER);
+		System.out.println(MIN_NUMBER);
+
+	}
+
+}
