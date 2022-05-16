1c1ab838a2adfe365ed075ff866da2e2e0cd8a0e
qhtjd8899 <qhtjd8899@naver.com>
2022-05-16 23:39:32 +0900

    숙제

--- JavaStudy_bosung/src/j04_입력/Input01.java
+++ JavaStudy_bosung/src/j04_입력/Input01.java
@@ -15,6 +15,19 @@ public class Input01 {
 		/*
 		 * 숙제: 스캐너 이용해서 윤년 만들어보기
 		 */
+		
+		System.out.print("년도 입력: ");
+		int year = scanner.nextInt();
+		
+		String result = null;
+		result = year % 4 == 0 
+				&& year % 100 != 0 
+				|| year % 400 == 0 ? "윤년" : "윤년이 아님";
+		
+		System.out.println(result);
+		
+		
+		
 	}
 
 }
