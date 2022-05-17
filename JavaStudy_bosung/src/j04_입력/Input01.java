962f6f31a2394a0985fa15bd1c14ef2277db7718
qhtjd8989 <qhtjd8899@naver.com>
2022-05-17 21:53:02 +0900

    j03_연산 8. 삼항연산자 복습
    j04_입력
    2. 스캐너 연습
    3. 스캐너 문제1 (next, nextLine 설명)
    4. 스캐너 문제2
    5. 스캐너, 삼항연산자 문제

--- JavaStudy_bosung/src/j04_입력/Input01.java
+++ JavaStudy_bosung/src/j04_입력/Input01.java
@@ -7,24 +7,34 @@ public class Input01 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.print("값 입력: "); // 입력을 받을때는 println이 아니라 print
-		int num = scanner.nextInt();
+		System.out.print("첫번째 값 입력: "); // 입력을 받을때는 println이 아니라 print
+		int num1 = scanner.nextInt();
+		System.out.print("두번째 값 입력: ");
+		int num2 = scanner.nextInt();
 		
-		System.out.println("입력한 값: " + num);
+		System.out.println("입력한 값: " + num1);
+		System.out.println("두값을 더한 값: " + (num1 + num2));
+		
+		scanner.next();				//문자열(String)
+		scanner.nextLine();			//문자열(String)
+		scanner.next().charAt(0);	//문자(char)
+		scanner.nextInt();			//정수(int)
+		scanner.nextDouble();		//실수(double)
+		
 		
 		/*
 		 * 숙제: 스캐너 이용해서 윤년 만들어보기
 		 */
 		
-		System.out.print("년도 입력: ");
-		int year = scanner.nextInt();
-		
-		String result = null;
-		result = year % 4 == 0 
-				&& year % 100 != 0 
-				|| year % 400 == 0 ? "윤년" : "윤년이 아님";
-		
-		System.out.println(result);
+//		System.out.print("년도 입력: ");
+//		int year = scanner.nextInt();
+//		
+//		String result = null;
+//		result = year % 4 == 0 
+//				&& year % 100 != 0 
+//				|| year % 400 == 0 ? "윤년" : "윤년이 아님";
+//		
+//		System.out.println(result);
 		
 		
 		
