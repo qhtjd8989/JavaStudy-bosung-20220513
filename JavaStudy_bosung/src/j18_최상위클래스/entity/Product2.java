b71a31899e51747376916984e820e4f53686626c
qhtjd8989 <qhtjd8899@naver.com>
2022-06-02 20:16:30 +0900

    nonnull사용방법

--- JavaStudy_bosung/src/j18_최상위클래스/entity/Product2.java
+++ JavaStudy_bosung/src/j18_최상위클래스/entity/Product2.java
@@ -7,18 +7,21 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @NoArgsConstructor
+@RequiredArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode
 @ToString
 public class Product2 {
-	
 	private int productCode;
+	@NonNull
 	private String productName;
 	private int category;
 	private LocalDate productionDate;
